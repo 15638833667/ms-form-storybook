2868ec51ac7a65b01b27a15fa2c780f14e038c78
王伟 <wangwei@ms-battery.cn>
2024-05-17 14:35:57 +0800

    feat 集成storybook

--- packages/msButton/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import MsButton from './MsButton.vue'
-
-// 使用install方法，在app.use挂载
-MsButton.install = (app: App) => {
-  app.component(MsButton.__name as string, MsButton)
-}
-
-export default MsButton
